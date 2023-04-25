b63e5b410f5876f83175a740670454a8e37d9a84
ZakiMohammed <shaikhzaki67@gmail.com>
2023-04-25 18:22:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='container my-3'>
+      <div className='row'>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
